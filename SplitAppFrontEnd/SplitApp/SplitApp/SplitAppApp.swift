cf82c3041788339505e103450005f9a4406d2f4a
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2022-10-11 22:00:17 -0400

    Added the front end file

---
+++ SplitAppFrontEnd/SplitApp/SplitApp/SplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitAppApp.swift
+//  SplitApp
+//
+//  Created by John Johnston on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
