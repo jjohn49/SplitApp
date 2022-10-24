a4a7419fff0b8b2215ac0c9a5463c8e8b3e03765
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2022-10-23 21:15:27 -0400

    Added a function for the api

--- SplitAppFrontEnd/SplitApp/SplitApp/EnviormentVariables.swift
+++ SplitAppFrontEnd/SplitApp/SplitApp/EnviormentVariables.swift
@@ -25,4 +25,23 @@ class EnviormentVariables: ObservableObject{
         Trip(id: "1", name: "Trip", users: ["me"], startDate: Date.now, endDate: Date.now),
         Trip(id: "1", name: "Trip", users: ["me"], startDate: Date.now, endDate: Date.now)
     ]
+    
+    //Need to change this 
+    func callAPI(){
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else{
+            return
+        }
+
+
+        let task = URLSession.shared.dataTask(with: url){
+            data, response, error in
+            
+            if let data = data, let string = String(data: data, encoding: .utf8){
+                print(string)
+            }
+        }
+
+        task.resume()
+    }
+    
 }
